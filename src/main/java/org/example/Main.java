42a1ea741afbe60a1c3041180322362b5bd6f053
Valeria Medina <valeriam200305@gamil.com>
2025-02-26 22:50:36 -0500

    Ejercicio de productos de maquina expendedora

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,60 +1,70 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
-        double tempIngresada;
-        double resultado=0;
+        double total=0;
+        double precioProductos = 0;
+        double dineroIngresado;
+        int productosAdquiridos=0;
+        double vueltas;
         int opcion;
 
         do{
-            System.out.println("SELCCIONE UNA OPCION:");
-            System.out.println("1. Farenheit a Celcius");
-            System.out.println("2. Celcius a Farenheit");
-            System.out.println("3. Kelvin a Celcius");
-            System.out.println("4. Celsius a Kelvin");
-            System.out.println("5. Farenheit a Kelvin");
-            System.out.println("6. Kelvin a Farenheit");
-            System.out.println("7. Rankine a Farenheit");
-            System.out.println("8. Farenheit a Rankine");
-            System.out.println("9. Reaumur a Celsius");
-            System.out.println("10. Celsius a Reaumur");
-            System.out.println("11. Rankine a Kelvin");
-            System.out.println("12. Kelvin a Rankine");
-            System.out.println("13. Rankine a Celsius");
-            System.out.println("14. Celsius a Rankine");
-            System.out.println("15. Celsius a Reaumur");
-            System.out.println("16. Reaumur a Celsius");
+            System.out.println("PRODUCTOS:");
+            System.out.println("1. Galletas Oreo - $2000");
+            System.out.println("2. Coca cola - $3000");
+            System.out.println("3. Agua - $2000");
+            System.out.println("4. Tostacos - $1300");
+            System.out.println("5. Papas pollo - $2200");
+            System.out.println("6. Te - $3000");
+            System.out.println("7. Chocolatina - $1550");
+            System.out.println("8. Manimoto - $2000");
+            System.out.println("9. Jugo hit - $1700");
+            System.out.println("10. Cheetos - $2300");
+            System.out.println("11. Terminar compra");
+            System.out.println("SELECCIONE NUMERO DEL PRODUCTO:");
             opcion = scanner.nextInt();
 
-            System.out.print("Ingrese la temperatura: ");
-            tempIngresada = scanner.nextDouble();
+            switch (opcion) {
 
-        resultado= switch (opcion) {
-                case 1 -> (5*(tempIngresada - 32)/9);
-                case 2 -> (9 * tempIngresada / 5) + 32;
-                case 3 -> tempIngresada - 273.15;
-                case 4 -> tempIngresada + 273.15;
-                case 5 -> (5*(tempIngresada-32)/9)+273.15;
-                case 6 -> (9*(tempIngresada-273.15)/5)+32;
-                case 7 -> tempIngresada - 459.67;
-                case 8 -> tempIngresada + 459.67;
-                case 9 -> (5* tempIngresada)/4;
-                case 10 -> (4 * tempIngresada)/5;
-                case 11 -> (5*(tempIngresada-491.67)/9)+273.15;
-                case 12 -> (9*(tempIngresada-273.15)/5)+491.67;
-                case 13 -> (5*(tempIngresada-491.67)/9);
-                case 14 -> ((9*tempIngresada)/5)+491.67;
-                case 15 -> (4*tempIngresada)/5;
-                case 16 -> (5*tempIngresada)/4;
-            default -> 0;
-        };
-            System.out.println("Resultado: " + resultado);
-
-        } while (opcion != 16);
-    }
+                case 1 -> precioProductos = 2000;
+                case 2 -> precioProductos = 3000;
+                case 3 -> precioProductos = 2000;
+                case 4 -> precioProductos = 1300;
+                case 5 -> precioProductos = 2200;
+                case 6 -> precioProductos = 3000;
+                case 7 -> precioProductos = 1550;
+                case 8 -> precioProductos = 2000;
+                case 9 -> precioProductos = 1700;
+                case 10 -> precioProductos = 2300;
+                case 11 -> System.out.println("Terminando compra");
+                default -> System.out.println("Opción inválida, intente de nuevo.");
+            }
+            if (opcion >= 1 && opcion <= 10){
+                total += precioProductos;
+                productosAdquiridos++;
+                System.out.println("Producto añadido - Total compra: $"+total);
+            }
+    }while (opcion!= 11 && productosAdquiridos < 5);
+        
+        if (total > 0){
+            System.out.println("Ingrese dinero");
+            dineroIngresado = scanner.nextDouble();
+            
+            if (dineroIngresado >= total){
+                vueltas = dineroIngresado - total;
+                System.out.println("Pago realizado - sus vueltas son: $"+ vueltas);
+            }else {
+                System.out.println("Dinero insuficiente, vuelva a intentar");
+            }
+        }else{
+            System.out.println("Saliendo");
+        }
+}
 }
