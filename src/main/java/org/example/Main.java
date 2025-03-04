43249ce05e92f8840abf1cfa0fe12490c268bf7a
Valeria Medina <valeriam200305@gamil.com>
2025-03-03 23:41:22 -0500

    Ejercicio de daño sobre pokemones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,44 +8,73 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int atacante;
+        int defensor;
+        int ataque;
+        int defensa;
+        double efectividad = 1.0;
 
-        int persona1;
-        int persona2;
-        int winJugador1 = 0;
-        int winJugador2 = 0;
+        do {
+            System.out.println("Seleccione el tipo del Pokémon atacante:");
+            System.out.println("1. Agua");
+            System.out.println("2. Fuego");
+            System.out.println("3. Planta");
+            System.out.println("4. Eléctrico");
+            atacante = scanner.nextInt();
 
-        Random random = new Random();
-        System.out.println("Piedra, papel o tijera...");
-        System.out.println("Recuerda que 1 es piedra - 2 es papel - 3 es tijera");
+            System.out.println("Seleccione el tipo del Pokémon defensor:");
+            System.out.println("1. Agua");
+            System.out.println("2. Fuego");
+            System.out.println("3. Planta");
+            System.out.println("4, Eléctrico");
+            defensor = scanner.nextInt();
 
-        while (winJugador1 < 2 && winJugador2 < 2) {
-            persona1 = random.nextInt(3) + 1;
-            persona2 = random.nextInt(3) + 1;
+        } while (defensor < 1 || defensor > 4 || atacante < 1 || atacante > 4);
 
-            System.out.println("Ronda N°:" + (winJugador1 + winJugador2 + 1) );
-            System.out.println("Jugadr 1 eligió: " + (persona1));
-            System.out.println("Jugador 2 eligió: " + (persona2));
+        do {
+            System.out.println("Ingrese el valor de ataque (1-100):");
+            ataque = scanner.nextInt();
 
-            if ((persona1 == 1 && persona2 == 2) ||
-                    (persona1 == 2 && persona2 == 3) ||
-                    (persona1 == 3 && persona2 == 1)) {
-                winJugador2++;
-                System.out.println("Ganador de ronda: Jugador N°2");
+            System.out.println("Ingrese el valor de defensa (1-100):");
+            defensa = scanner.nextInt();
 
-            } else if (persona1 == persona2) {
-                System.out.println("Empate");
-            } else {
-                winJugador1++;
-                System.out.println("Ganador de ronda: Jugador N°1");
+        } while (defensa < 1 || defensa > 100 || ataque < 1 || ataque > 100);
+
+        switch (atacante) {
+            case 1 -> {
+                switch (defensor) {
+                    case 2 -> efectividad = 2.0;
+                    case 3 -> efectividad = 0.5;
+                    case 4 -> efectividad = 1.0;
+                }
             }
+            case 2 -> {
+                switch (defensor) {
+                    case 3 -> efectividad = 2.0;
+                    case 1 -> efectividad = 0.5;
+                    case 4 -> efectividad = 1.0;
+                }
+            }
+            case 3 -> {
+                switch (defensor) {
+                    case 1 -> efectividad = 2.0;
+                    case 2 -> efectividad = 0.5;
+                    case 4 -> efectividad = 2.0;
+                }
+            }
+            case 4 -> {
+                switch (defensor) {
+                    case 1 -> efectividad = 2.0;
+                    case 4 -> efectividad = 0.5;
+                    case 3 -> efectividad = 0.5;
+                }
+            }
+            default -> efectividad = 1.0;
         }
 
+        double daño = 50 * ((double) ataque / defensa) * efectividad;
+        System.out.println("El daño del ataque es: " + daño);
 
-        if (winJugador1 == 2) {
-            System.out.println("El ganador del juego es el Jugador 1");
-        } else {
-            System.out.println("El ganador del juego es el Jugador 2");
-
-
-        }
-    }    }
+    }
+}
